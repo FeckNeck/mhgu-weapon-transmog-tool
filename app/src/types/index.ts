d80ecd1c806c8c35595f3ae3244772775bc4e368
FeckNeck <mathis.dousse@outlook.fr>
2025-02-22 08:40:33 +0100

    feat: update conf

--- app/src/types/index.ts
+++ app/src/types/index.ts
@@ -1,13 +1,14 @@
 export type Weapons = Weapon[];
 
 export interface Weapon {
-  name: string;
   id: string;
+  name: string;
+  icon: string;
   image_prefix: string;
   skins: Skin[];
 }
 
 export interface Skin {
-  name: string;
   id: string;
+  name: string;
 }
